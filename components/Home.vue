6ae7a274ccbf0f9de44788fb6ab3c959747a9932
József Tar <szurilo@gmail.com>
2022-08-27 23:12:03 +0200

    Configure tailwindcss via npx tailwindcss init

--- components/Home.vue
+++ components/Home.vue
@@ -2,7 +2,7 @@
   <div
     class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
   >
-    <img src="~assets/avatar.png" class="w-20 h-20 rounded-full" />
+    <img src="~assets/avatar.png" class="w-36 h-36 rounded-full" />
     <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
         <h2 class="text-2xl leading-7 font-semibold">
