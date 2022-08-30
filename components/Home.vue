34d278df4d78e9e8a1477e6a57bad1379a54c113
Tar József <jozsef.tar@atollgroup.eu>
2022-08-30 16:58:17 +0200

    edit Home page

--- components/Home.vue
+++ components/Home.vue
@@ -1,36 +1,24 @@
 <template>
-  <div
-    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
-  >
-    <img src="~assets/avatar.png" class="w-36 h-36 rounded-full" />
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
     <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
-      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+      <div class="mt-8 overflow-hidden p-6">
+        <img src="~assets/avatar.png" class="w-36 h-36 rounded-full float-right" />
         <h2 class="text-2xl leading-7 font-semibold">
-          Welcome to your Nuxt Application
+          Hi, 
+          <br>
+          my name is József.
         </h2>
-        <p class="mt-3 text-gray-600">
-          We recommend you take a look at the
-          <a
-            href="https://nuxtjs.org"
-            target="_blank"
-            class="button--doc text-green-500 hover:underline"
-            >Nuxt documentation</a
-          >, whether you are new or have previous experience with the
-          framework.<br />
-        </p>
-        <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
-          To get started, remove
-          <code class="bg-gray-100 text-sm p-1 rounded border"
-            >components/Tutorial.vue</code
-          >
-          and start coding in
-          <code class="bg-gray-100 text-sm p-1 rounded border"
-            >pages/index.vue</code
-          >. Have fun!
-        </p>
       </div>
+
+      <p class="mt-3 text-gray-600">
+        I am full stack web developer. I love projects that make the world a better place.
+      </p>
+      <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
+        I have more than 10 years of experience in developing all kinds of software solutions. My expertiese is ...
+      </p>
+
       <div class="flex justify-center pt-4 space-x-2">
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank"
+        <a href="https://github.com/szurilo" target="_blank"
           ><svg
             class="w-6 h-6 text-gray-600 hover:text-gray-800 button--github"
             xmlns="http://www.w3.org/2000/svg"
